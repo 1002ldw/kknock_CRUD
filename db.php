48b5e3b1e84c281cc005546b2554e8581d40a44e
1002ldw <1002ldw@gmail.com>
2026-05-20 16:48:43 +0000

    db.php

---
+++ db.php
@@ -0,0 +1,14 @@
+<?php
+$host = "localhost";
+$user = "board_user";
+$pass = "your_password";
+$dbname = "board_db";
+
+$conn = new mysqli($host, $user, $pass, $dbname);
+
+if ($conn->connect_error) {
+    die("DB 연결 실패: " . $conn->connect_error);
+}
+
+$conn->set_charset("utf8mb4");
+?>
